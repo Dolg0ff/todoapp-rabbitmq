ce7db5d16de7664dee6747a94b81bbe138841456
Yuriy Dolgov <ys.dolgoff@gmail.com>
2025-01-18 12:07:13 +0300

    main & docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Dolg0ff/todoapp-rabbitmq/internal/config"
+	"github.com/Dolg0ff/todoapp-rabbitmq/internal/handler"
+	"github.com/Dolg0ff/todoapp-rabbitmq/internal/rabbitmq"
+	"github.com/Dolg0ff/todoapp-rabbitmq/internal/service"
+	"github.com/Dolg0ff/todoapp-rabbitmq/pkg/logger"
+	log "github.com/sirupsen/logrus"
+)
+
 func main() {
-	// Загрузка конфигурации
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger := logger.NewLogger(cfg.LogLevel)
+
+	conn, err := rabbitmq.NewConnection(cfg)
+	if err != nil {
+		logger.Fatal("Failed to connect to RabbitMQ", err)
+	}
+	defer conn.Close()
+
+	service := service.NewMessageService(logger)
+	handler := handler.NewMessageHandler(service, logger)
+	consumer, err := rabbitmq.NewConsumer(conn, handler, logger)
+	if err != nil {
+		logger.Fatal("Failed to create consumer", err)
+	}
+
+	if err := consumer.Start(); err != nil {
+		logger.Fatal("Failed to start consumer", err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+
+	logger.Info("Shutting down...")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/Dolg0ff/todoapp-rabbitmq
 
 go 1.23.2
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/rabbitmq/amqp091-go v1.10.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
