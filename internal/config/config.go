91d1715959673af5de363fe5ae546084312878e6
Yuriy Dolgov <ys.dolgoff@gmail.com>
2025-01-18 12:01:10 +0300

    log & config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,47 @@
 package config
 
-type Config struct {
-	RabbitMQ struct {
-		URL      string
-		Queue    string
-		Exchange string
-	}
-	LogLevel string
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type RabbitMQConfig struct {
+	URL      string `envconfig:"RABBITMQ_URL"`
+	Queue    string `envconfig:"RABBITMQ_QUEUE"`
+	Exchange string `envconfig:"RABBITMQ_EXCHANGE"`
 }
 
-// func LoadConfig() (*Config, error) {
+type Config struct {
+	RabbitMQ RabbitMQConfig
+	LogLevel string `envconfig:"LOG_LEVEL"`
+}
 
-// }
+func LoadConfig() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("error loading .env file: %w", err)
+	}
+
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, err
+	}
+
+	if cfg.RabbitMQ.URL == "" {
+		return nil, fmt.Errorf("RABBITMQ_URL is required")
+	}
+	if cfg.RabbitMQ.Queue == "" {
+		return nil, fmt.Errorf("RABBITMQ_QUEUE is required")
+	}
+	if cfg.RabbitMQ.Exchange == "" {
+		return nil, fmt.Errorf("RABBITMQ_EXCHANGE is required")
+	}
+	if cfg.LogLevel == "" {
+		return nil, fmt.Errorf("LOG_LEVEL is required")
+	}
+
+	fmt.Printf("Loaded config: %+v\n", cfg)
+
+	return &cfg, nil
+}
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,23 +1,129 @@
 package logger
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+)
+
 type Logger struct {
-	level string
+	log *logrus.Logger
 }
 
 func NewLogger(level string) *Logger {
+	l := logrus.New()
+
+	_, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("Failed to get current directory: %v\n", err)
+	}
+
+	logsDir := "logs"
+	if err := os.MkdirAll(logsDir, 0755); err != nil {
+		fmt.Printf("Failed to create logs directory: %v\n", err)
+	}
+
+	logPath := filepath.Join(logsDir, "app.log")
+	fmt.Printf("Log file path: %s\n", logPath)
+
+	file, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("Failed to open log file: %v\n", err)
+		l.SetOutput(os.Stdout)
+	} else {
+		fmt.Printf("Successfully opened log file\n")
+		l.SetOutput(io.MultiWriter(os.Stdout, file))
+	}
+
+	l.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+
+	switch level {
+	case "debug":
+		l.SetLevel(logrus.DebugLevel)
+	case "info":
+		l.SetLevel(logrus.InfoLevel)
+	case "warn":
+		l.SetLevel(logrus.WarnLevel)
+	case "error":
+		l.SetLevel(logrus.ErrorLevel)
+	default:
+		l.SetLevel(logrus.InfoLevel)
+	}
+
 	return &Logger{
-		level: level,
+		log: l,
 	}
 }
 
 func (l *Logger) Info(msg string, args ...interface{}) {
-	// Реализация логирования
+	if len(args) > 0 {
+		l.log.WithFields(convertArgsToFields(args...)).Info(msg)
+	} else {
+		l.log.Info(msg)
+	}
 }
 
 func (l *Logger) Error(msg string, err error, args ...interface{}) {
-	// Реализация логирования ошибок
+	fields := logrus.Fields{}
+	if err != nil {
+		fields["error"] = err.Error()
+	}
+
+	if len(args) > 0 {
+		for k, v := range convertArgsToFields(args...) {
+			fields[k] = v
+		}
+	}
+
+	l.log.WithFields(fields).Error(msg)
 }
 
 func (l *Logger) Fatal(msg string, err error, args ...interface{}) {
-	// Логирование и завершение программы
+	fields := logrus.Fields{}
+	if err != nil {
+		fields["error"] = err.Error()
+	}
+
+	if len(args) > 0 {
+		for k, v := range convertArgsToFields(args...) {
+			fields[k] = v
+		}
+	}
+
+	l.log.WithFields(fields).Fatal(msg)
+}
+
+// Вспомогательная функция для конвертации аргументов в поля
+func convertArgsToFields(args ...interface{}) logrus.Fields {
+	fields := logrus.Fields{}
+
+	// Конвертируем аргументы в пары ключ-значение
+	for i := 0; i < len(args); i += 2 {
+		if i+1 < len(args) {
+			fields[args[i].(string)] = args[i+1]
+		}
+	}
+
+	return fields
+}
+
+func (l *Logger) Debug(msg string, args ...interface{}) {
+	if len(args) > 0 {
+		l.log.WithFields(convertArgsToFields(args...)).Debug(msg)
+	} else {
+		l.log.Debug(msg)
+	}
+}
+
+func (l *Logger) Warn(msg string, args ...interface{}) {
+	if len(args) > 0 {
+		l.log.WithFields(convertArgsToFields(args...)).Warn(msg)
+	} else {
+		l.log.Warn(msg)
+	}
 }
